99f6a983501b69eb4b4450d6a8b4439c7161afc1
KEYBOSS <yokeshwaran.yg@gmail.com>
2025-05-21 19:30:59 +0530

    Delete new.py

--- new.py
+++
@@ -1,7 +0,0 @@
-def emp():
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
-    place = input("Enter your place: ")
-    experience = int(input("Enter your experience: "))
-    if(experience<1):
-        print(this your 
